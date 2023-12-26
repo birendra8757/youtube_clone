c4ea7b59b1e7c6fbb2758ebb527ff844c68a2c9c
birendra8757 <computersinghaniya@gmail.com>
2023-12-26 23:47:18 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+
+const app = express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+//for accept json request limit
+app.use(express.json({limit:"16kb"}))
+//for accept url 
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
